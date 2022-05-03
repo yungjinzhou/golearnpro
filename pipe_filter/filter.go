d0431e10d76c47ed883408e6d1cb7ea6be60dc97
yung <yjz@192.168.0.105>
2022-05-03 15:22:44 +0800

    pipe filter

---
+++ pipe_filter/filter.go
@@ -0,0 +1,11 @@
+package pipefilter
+
+type Request interface {
+}
+
+type Response interface {
+}
+
+type Filter interface {
+	Process(data Request) (Response, error)
+}
---
+++ pipe_filter/to_int_filter.go
@@ -0,0 +1,34 @@
+package pipefilter
+
+import (
+	"errors"
+	"strconv"
+)
+
+var ToIntFliterWrongFormatError = errors.New("input data should be []string")
+
+type ToIntFilter struct {
+}
+
+func NewToIntFilter() *ToIntFilter {
+	return &ToIntFilter{}
+}
+
+func (tif *ToIntFilter) Process(data Request) (Response, error) {
+	parts, ok := data.([]string)
+	if !ok {
+		return nil, ToIntFliterWrongFormatError
+	}
+	ret := []int{}
+	for _, part := range parts {
+		s, err := strconv.Atoi(part)
+		if err != nil {
+			return nil, err
+		}
+		ret = append(ret, s)
+	}
+	return ret, nil
+}
+
+
+
