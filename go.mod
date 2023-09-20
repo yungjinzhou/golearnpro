fcbe00d141ddd742a0682c7955100cd30ca39a46
yungjinzhou <yungjinzhou@163.com>
2023-09-20 14:56:10 +0800

    trienode

--- go.mod
+++ go.mod
@@ -5,6 +5,8 @@ go 1.17
 require (
 	github.com/easierway/concurrent_map v1.0.0
 	github.com/go-redis/redis v6.15.9+incompatible
+	github.com/go-redis/redis/v8 v8.11.5
+	github.com/google/uuid v1.3.1
 	github.com/julienschmidt/httprouter v1.3.0
 	github.com/mailru/easyjson v0.7.7
 	github.com/smartystreets/goconvey v1.7.2
@@ -15,12 +17,9 @@ require (
 	github.com/cespare/xxhash/v2 v2.1.2 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
-	github.com/go-redis/redis/v8 v8.11.5 // indirect
-	github.com/google/uuid v1.3.1 // indirect
 	github.com/gopherjs/gopherjs v0.0.0-20181017120253-0766667cb4d1 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/jtolds/gls v4.20.0+incompatible // indirect
-	github.com/onsi/ginkgo v1.16.5 // indirect
 	github.com/onsi/gomega v1.19.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/smartystreets/assertions v1.2.0 // indirect
--- trie_node/trienode.go
+++ trie_node/trienode.go
@@ -1,4 +1,6 @@
-package trie_node
+package main
+
+import "fmt"
 
 // 以26个字母为前提测试
 type trieNode struct {
@@ -13,4 +15,109 @@ type Trie struct {
 
 func NewTrie() *Trie {
 	return &Trie{root: &trieNode{}}
+}
+
+func (t *Trie) Search(word string) bool {
+	// 查找目标节点，使得从根节点开始抵达目标节点沿路字符形成的字符串恰好等于 word
+	node := t.search(word)
+	return node != nil && node.end
+}
+
+func (t *Trie) search(target string) *trieNode {
+	// 移动指针从根节点出发
+	move := t.root
+	// 依次遍历 target 中的每个字符
+	for _, ch := range target {
+		// 倘若 nexts 中不存在对应于这个字符的节点，说明该单词没插入过，返回 nil
+		if move.nexts[ch-'a'] == nil {
+			return nil
+		}
+		// 指针向着子节点移动
+		move = move.nexts[ch-'a']
+	}
+
+	// 来到末尾，说明已经完全匹配好单词，直接返回这个节点
+	// 需要注意，找到目标节点不一定代表单词存在，因为该节点的 end 标识未必为 true
+	// 比如我们之前往 trie 中插入了 apple 这个单词，但是查找 app 这个单词时，预期的返回结果应该是不存在，此时就需要使用到 end 标识 进行区分
+	return move
+}
+
+func (t *Trie) StartsWith(prefix string) bool {
+	return t.search(prefix) != nil
+}
+
+func (t *Trie) PassCnt(prefix string) int {
+	node := t.search(prefix)
+	if node == nil {
+		return 0
+	}
+	return node.passCnt
+}
+
+func (t *Trie) Insert(word string) {
+	if t.Search(word) {
+		return
+	}
+
+	move := t.root
+
+	for _, ch := range word {
+		if move.nexts[ch-'a'] == nil {
+			move.nexts[ch-'a'] = &trieNode{}
+		}
+		move.nexts[ch-'a'].passCnt++
+		move = move.nexts[ch-'a']
+	}
+
+	move.end = true
+}
+
+func (t *Trie) Erase(word string) bool {
+	if !t.Search(word) {
+		return false
+	}
+
+	move := t.root
+	for _, ch := range word {
+		move.nexts[ch-'a'].passCnt--
+		if move.nexts[ch-'a'].passCnt == 0 {
+			move.nexts[ch-'a'] = nil
+			return true
+		}
+		move = move.nexts[ch-'a']
+	}
+
+	move.end = false
+	return true
+}
+
+func main() {
+	trie := NewTrie()
+
+	// 插入单词
+	trie.Insert("apple")
+	trie.Insert("banana")
+	trie.Insert("orange")
+
+	// 搜索单词是否存在
+	fmt.Println(trie.Search("apple"))  // 输出: true
+	fmt.Println(trie.Search("banana")) // 输出: true
+	fmt.Println(trie.Search("orange")) // 输出: true
+	fmt.Println(trie.Search("grape"))  // 输出: false
+
+	// 判断前缀是否存在
+	fmt.Println(trie.StartsWith("app")) // 输出: true
+	fmt.Println(trie.StartsWith("ban")) // 输出: true
+	fmt.Println(trie.StartsWith("ora")) // 输出: true
+	fmt.Println(trie.StartsWith("gr"))  // 输出: false
+
+	// 获取前缀出现的次数
+	fmt.Println(trie.PassCnt("app")) // 输出: 1
+	fmt.Println(trie.PassCnt("ban")) // 输出: 1
+	fmt.Println(trie.PassCnt("ora")) // 输出: 1
+	fmt.Println(trie.PassCnt("gr"))  // 输出: 0
+
+	// 删除单词
+	fmt.Println(trie.Erase("apple"))  // 输出: true
+	fmt.Println(trie.Search("apple")) // 输出: false
 }
